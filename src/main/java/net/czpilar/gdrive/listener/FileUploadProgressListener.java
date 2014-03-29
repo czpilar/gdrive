45713273ee6aa3be35e1897f1da6e8420f4c8a84
czpilar <david@czpilar.net>
2014-03-29 19:52:45 +0100

    First version

---
+++ src/main/java/net/czpilar/gdrive/listener/FileUploadProgressListener.java
@@ -0,0 +1,41 @@
+package net.czpilar.gdrive.listener;
+
+import java.io.IOException;
+import java.text.NumberFormat;
+
+import com.google.api.client.googleapis.media.MediaHttpUploader;
+import com.google.api.client.googleapis.media.MediaHttpUploaderProgressListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author David Pilař (david@czpilar.net)
+ */
+public class FileUploadProgressListener implements MediaHttpUploaderProgressListener {
+
+	private static final Logger LOG = LoggerFactory.getLogger(FileUploadProgressListener.class);
+
+	private final String filename;
+
+	public FileUploadProgressListener(String filename) {
+		this.filename = filename;
+	}
+
+	@Override
+	public void progressChanged(MediaHttpUploader uploader) throws IOException {
+		switch (uploader.getUploadState()) {
+			case INITIATION_STARTED:
+				LOG.info("Started initiation uploading file {}", filename);
+				break;
+			case INITIATION_COMPLETE:
+				LOG.info("Finished initiation uploading file {}", filename);
+				break;
+			case MEDIA_IN_PROGRESS:
+				LOG.info("Uploaded {} of file {}", NumberFormat.getPercentInstance().format(uploader.getProgress()), filename);
+				break;
+			case MEDIA_COMPLETE:
+				LOG.info("Finished uploading file {}", filename);
+				break;
+		}
+	}
+}
---
+++ src/main/java/net/czpilar/gdrive/service/impl/GDriveService.java
@@ -0,0 +1,141 @@
+package net.czpilar.gdrive.service.impl;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
+import com.google.api.client.http.FileContent;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.model.File;
+import com.google.api.services.drive.model.ParentReference;
+import net.czpilar.gdrive.constant.GDriveConstants;
+import net.czpilar.gdrive.exception.AuthorizationFailedException;
+import net.czpilar.gdrive.exception.FileSaveException;
+import net.czpilar.gdrive.listener.FileUploadProgressListener;
+import net.czpilar.gdrive.service.IGDriveAuthorizationService;
+import net.czpilar.gdrive.service.IGDriveFileService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author David Pilař (david@czpilar.net)
+ */
+public class GDriveService extends AbstractGDriveService implements IGDriveAuthorizationService, IGDriveFileService {
+
+	private static final Logger LOG = LoggerFactory.getLogger(GDriveService.class);
+
+	public GDriveService(String propertyFile) {
+		super(propertyFile);
+	}
+
+	@Override
+	public String getAuthorizationURL() {
+		return getFlow().newAuthorizationUrl().setRedirectUri(GDriveConstants.REDIRECT_URI).build();
+	}
+
+	@Override
+	public Credential authorize(String authorizationCode) {
+		try {
+			GoogleTokenResponse response = getFlow().newTokenRequest(authorizationCode).setRedirectUri(GDriveConstants.REDIRECT_URI).execute();
+			Credential credential = getFlow().createAndStoreCredential(response, null);
+			storeCredential(credential);
+			return credential;
+		} catch (IOException e) {
+			throw new AuthorizationFailedException("Error occures during authorization process.", e);
+		}
+	}
+
+	private File findParent(String parentName) throws IOException {
+		List<File> items = getDrive().files().list().setQ("title='" + parentName + "' and mimeType = 'application/vnd.google-apps.folder'").execute().getItems();
+		for (File item : items) {
+			if (!Boolean.TRUE.equals(item.getExplicitlyTrashed())) {
+				for (ParentReference reference : item.getParents()) {
+					if (reference.getIsRoot()) {
+						return item;
+					}
+				}
+			}
+		}
+		return null;
+	}
+
+	private File findOrCreateParent(String parentDir) {
+		File parent = null;
+		if (parentDir != null) {
+			try {
+				parent = findParent(parentDir);
+				if (parent == null) {
+					File body = new File();
+					body.setTitle(parentDir);
+					body.setMimeType("application/vnd.google-apps.folder");
+					parent = getDrive().files().insert(body).execute();
+				}
+			} catch (IOException e) {
+				LOG.warn("Cannot find or create parent dir. Using root dir.", e);
+			}
+		}
+		return parent;
+	}
+
+	@Override
+	public File uploadFile(String filename, String parentDir) {
+		return uploadFile(filename, findOrCreateParent(getUploadDir(parentDir)));
+	}
+
+	@Override
+	public File uploadFile(String filename, File parent) {
+		LOG.info("Uploading file {}", filename);
+		try {
+			Path path = Paths.get(filename);
+
+			File body = new File();
+			body.setTitle(path.getFileName().toString());
+			body.setMimeType(Files.probeContentType(path));
+			if (parent != null) {
+				body.setParents(Arrays.asList(new ParentReference().setId(parent.getId())));
+			}
+
+			Drive.Files.Insert insert = getDrive().files().insert(body, new FileContent(body.getMimeType(), path.toFile()));
+			insert.getMediaHttpUploader().setDirectUploadEnabled(false);
+			insert.getMediaHttpUploader().setProgressListener(new FileUploadProgressListener(filename));
+			File file = insert.execute();
+			LOG.info("Finished uploading file {} - remote file ID is {}", filename, file.getId());
+			return file;
+		} catch (IOException e) {
+			LOG.error("File {} was not uploaded.", filename);
+			throw new FileSaveException("File was not uploaded.", e);
+		}
+	}
+
+	@Override
+	public File uploadFile(String filename) {
+		return uploadFile(filename, (File) null);
+	}
+
+	@Override
+	public void uploadFiles(List<String> filenames) {
+		uploadFiles(filenames, (File) null);
+	}
+
+	@Override
+	public void uploadFiles(List<String> filenames, String parentDir) {
+		uploadFiles(filenames, findOrCreateParent(getUploadDir(parentDir)));
+
+	}
+
+	@Override
+	public void uploadFiles(List<String> filenames, File parent) {
+		for (String filename : filenames) {
+			try {
+				uploadFile(filename, parent);
+			} catch (FileSaveException e) {
+				LOG.error("Error during uploading file.", e);
+			}
+		}
+	}
+}
